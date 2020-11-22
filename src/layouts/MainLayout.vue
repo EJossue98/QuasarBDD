e37c24cdb4751d1c5d73be52eba50f57242455db
EJossue98 <erick98-jossue@hotmail.com>
2020-11-22 13:43:17 -0500

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+
+
+       
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md text-center">
+        <div class="text-h3">APLICACIÓN LISTA DE COSAS POR HACER</div>
+      </div>
+    </q-header>
+
+      <q-footer class="bg-white" bordered>
+      <q-tabs class="text-grey-10" active-color="primary" indicator-color="primary">
+        <q-route-tab name="Camera" icon="home" label="Home" to="/home" exact/>
+        <q-route-tab name="Lista" icon="mail" label="Lista" to="/" exact/>
+
+      </q-tabs>
+      </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+import { date } from 'quasar'
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  },
+}
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex column">
+    
+      <div class="col column text-center text-white">
+        
+        <div class="col text-h1  text-center">
+          Quasar 
+        </div>
+
+        <img src="../statics/ToDo.jpg" class="center" alt="ToDo" width="425" height="380" >
+
+      </div>
+  </q-page>
+</template>
+
+
+<style lang="sass">
+.q-page
+  background: #061700
+.degree
+  top: -44px
+</style>
